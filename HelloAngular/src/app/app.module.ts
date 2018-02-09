310f47b535a235103eaca38ef1ecf3720cb8b7cd
kongyufei <kongyufei5039@outlook.com>
2018-02-09 17:23:12 +0800

    父子组件间的通讯

--- HelloAngular/src/app/app.module.ts
+++ HelloAngular/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ParentToChildComponent } from './parent-to-child/parent-to-child.component';
+import { ChildComponent } from './parent-to-child/child/child.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ParentToChildComponent
+    ParentToChildComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule
